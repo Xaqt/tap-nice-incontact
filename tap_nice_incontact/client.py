5806b6f570b318ba4407f55aba729372b9b7b949
agiannotti <aj.gianno@gmail.com>
2022-12-14 22:05:54 -0600

    uri adjustments

--- tap_nice_incontact/client.py
+++ tap_nice_incontact/client.py
@@ -8,10 +8,10 @@ from singer import get_logger
 LOGGER = get_logger()
 
 API_AUTH_DOMAIN = 'na1'
-API_AUTH_URI = 'https://{}.incontact.com/authentication/v1/token/access-key'
-API_REFRESH_URI = 'https://{}.incontact.com/public/user/refresh'
-API_BASE_URI = 'https://api-{}.incontact.com/inContactAPI/services/v{}'
-API_VERSION = '21.0'
+API_AUTH_URI = 'https://api.incontact.com/InContactAuthorizationServer/token/access-key'
+API_REFRESH_URI = 'https://api-b2.incontact.com/public/user/refresh'
+API_BASE_URI = 'https://api-b2.incontact.com/inContactAPI/services/v22.0/interactions'
+API_VERSION = '22.0'
 MAX_RETRIES = 5
 
 
